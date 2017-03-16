dd6ffeae1842a7684fd81e35393b80e2c502bb03
Craig Gidney <craiggidney@google.com>
2017-03-15 20:34:26 -0400

    Fix cpp-simulator not being installed causing many unrelated tests to fail - Reduce to a single dedicated test to check for the problem - Only run other tests on available simulators

--- projectq/backends/_sim/_simulator_test.py
+++ projectq/backends/_sim/_simulator_test.py
@@ -31,7 +31,24 @@ from projectq.meta import Control
 from projectq.backends import Simulator
 
 
-@pytest.fixture(params=["cpp_simulator","py_simulator"])
+def test_is_cpp_simulator_present():
+    import projectq.backends._sim._cppsim
+    assert projectq.backends._sim._cppsim
+
+
+def get_available_simulators():
+    result = []
+    try:
+        import projectq.backends._sim._cppsim as _
+        result.append("cpp_simulator")
+    except ImportError as ex:
+        # The C++ simulator was either not installed or is misconfigured. Skip.
+        pass
+    result.append("py_simulator")
+    return result
+
+
+@pytest.fixture(params=get_available_simulators())
 def sim(request):
 	if request.param == "cpp_simulator":
 		from projectq.backends._sim._cppsim import Simulator as CppSim
