2465dc4126608453fb9ffff0d1f4c569b8683c02
Craig Gidney <craiggidney@google.com>
2017-05-05 20:58:55 -0700

    test fix

--- projectq/cengines/_testengine_test.py
+++ projectq/cengines/_testengine_test.py
@@ -27,9 +27,9 @@ def test_compare_engine_str():
     H | qb0
     CNOT | (qb0, qb1)
     eng.flush()
-    expected = ("Qubit 0 : Allocate | Qubit[0], H | Qubit[0], " +
-                "CX | ( Qubit[0], Qubit[1] )\nQubit 1 : Allocate | Qubit[1]," +
-                " CX | ( Qubit[0], Qubit[1] )\n")
+    expected = ("Qubit 0 : Allocate | Qureg[0], H | Qureg[0], " +
+                "CX | ( Qureg[0], Qureg[1] )\nQubit 1 : Allocate | Qureg[1]," +
+                " CX | ( Qureg[0], Qureg[1] )\n")
     assert str(compare_engine) == expected
 
 
