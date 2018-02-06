8471c45b5d805ac9ba9411a649b8ad4a40d5473b
Thomas Haener <thomas_haener@hotmail.com>
2018-02-06 09:13:33 +0100

    Add fix and test for emulation of controlled time evolution.

--- projectq/backends/_sim/_cppkernels/simulator.hpp
+++ projectq/backends/_sim/_cppkernels/simulator.hpp
@@ -335,6 +335,7 @@ public:
         unsigned s = std::abs(time) * op_nrm + 1.;
         complex_type correction = std::exp(-time * I * tr / (double)s);
         auto output_state = vec_;
+        auto ctrlmask = get_control_mask(ctrl);
         for (unsigned i = 0; i < s; ++i){
             calc_type nrm_change = 1.;
             for (unsigned k = 0; nrm_change > 1.e-12; ++k){
@@ -342,7 +343,7 @@ public:
                 auto current_state = vec_;
                 auto update = StateVector(vec_.size(), 0.);
                 for (auto const& tup : td){
-                    apply_term(tup.first, ids, ctrl);
+                    apply_term(tup.first, ids, {});
                     #pragma omp parallel for schedule(static)
                     for (std::size_t j = 0; j < vec_.size(); ++j){
                         update[j] += vec_[j] * tup.second;
@@ -354,14 +355,17 @@ public:
                 for (std::size_t j = 0; j < vec_.size(); ++j){
                     update[j] *= coeff;
                     vec_[j] = update[j];
-                    output_state[j] += update[j];
-                    nrm_change += std::norm(update[j]);
+                    if ((j & ctrlmask) == ctrlmask){
+                        output_state[j] += update[j];
+                        nrm_change += std::norm(update[j]);
+                    }
                 }
                 nrm_change = std::sqrt(nrm_change);
             }
             #pragma omp parallel for schedule(static)
             for (std::size_t j = 0; j < vec_.size(); ++j){
-                output_state[j] *= correction;
+                if ((j & ctrlmask) == ctrlmask)
+                    output_state[j] *= correction;
                 vec_[j] = output_state[j];
             }
         }
