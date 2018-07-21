e98e980cdef0678c3b55305abf625ceb841af6c6
Mathias Soeken <mathias.soeken@gmail.com>
2018-07-21 10:48:23 +0200

    Add RevKit interface (#241)

---
+++ examples/hws6.py
@@ -0,0 +1,39 @@
+from projectq.cengines import MainEngine
+from projectq.ops import All, H, X, CNOT, Measure
+from projectq.meta import Compute, Uncompute, Dagger
+from projectq.libs.revkit import PhaseOracle, PermutationOracle
+
+import revkit
+
+# phase function
+def f(a, b, c, d, e, f):
+    return (a and b) ^ (c and d) ^ (e and f)
+
+# permutation
+pi = [0, 2, 3, 5, 7, 1, 4, 6]
+
+eng = MainEngine()
+qubits = eng.allocate_qureg(6)
+x = qubits[::2]  # qubits on odd lines
+y = qubits[1::2] # qubits on even lines
+
+# circuit
+with Compute(eng):
+    All(H) | qubits
+    All(X) | [x[0], x[1]]
+    PermutationOracle(pi) | y
+PhaseOracle(f) | qubits
+Uncompute(eng)
+
+with Compute(eng):
+    with Dagger(eng):
+        PermutationOracle(pi, synth = revkit.dbs) | x
+PhaseOracle(f) | qubits
+Uncompute(eng)
+
+All(H) | qubits
+
+All(Measure) | qubits
+
+# measurement result
+print("Shift is {}".format(sum(int(q) << i for i, q in enumerate(qubits))))
---
+++ projectq/libs/revkit/_phase_test.py
@@ -0,0 +1,81 @@
+#   Copyright 2017 ProjectQ-Framework (www.projectq.ch)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""Tests for libs.revkit._phase."""
+
+import pytest
+
+from projectq.types import Qubit
+from projectq import MainEngine
+from projectq.backends import Simulator
+from projectq.cengines import DummyEngine
+from projectq.ops import All, H, Measure
+
+from projectq.libs.revkit import PhaseOracle
+
+import numpy as np
+
+# run this test only if RevKit Python module can be loaded
+revkit = pytest.importorskip('revkit')
+
+
+def test_phase_majority():
+    sim = Simulator()
+    main_engine = MainEngine(sim)
+
+    qureg = main_engine.allocate_qureg(3)
+    All(H) | qureg
+    PhaseOracle(0xe8) | qureg
+
+    main_engine.flush()
+
+    assert np.array_equal(np.sign(sim.cheat()[1]),
+                          [1., 1., 1., -1., 1., -1., -1., -1.])
+    All(Measure) | qureg
+
+
+def test_phase_majority_from_python():
+    dormouse = pytest.importorskip('dormouse')
+
+    def maj(a, b, c):
+        return (a and b) or (a and c) or (b and c)  # pragma: no cover
+
+    sim = Simulator()
+    main_engine = MainEngine(sim)
+
+    qureg = main_engine.allocate_qureg(3)
+    All(H) | qureg
+    PhaseOracle(maj) | qureg
+
+    main_engine.flush()
+
+    assert np.array_equal(np.sign(sim.cheat()[1]),
+                          [1., 1., 1., -1., 1., -1., -1., -1.])
+    All(Measure) | qureg
+
+
+def test_phase_invalid_function():
+    main_engine = MainEngine(backend=DummyEngine(),
+                             engine_list=[DummyEngine()])
+
+    qureg = main_engine.allocate_qureg(3)
+
+    with pytest.raises(AttributeError):
+        PhaseOracle(-42) | qureg
+
+    with pytest.raises(AttributeError):
+        PhaseOracle(0xcafe) | qureg
+
+    with pytest.raises(RuntimeError):
+        PhaseOracle(0x8e, synth=lambda: revkit.esopbs()) | qureg
