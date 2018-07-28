1f9a4f7a03c0091095cb522566658a29bec1d1e6
Fernando de la Iglesia <fernando.delaiglesia@gmail.com>
2018-07-28 19:31:36 +0200

    Initial version

--- examples/phase_estimation.py
+++ examples/phase_estimation.py
@@ -1,14 +1,13 @@
 from projectq.ops import H, X, Y, Z, Tensor, QFT, get_inverse
-from projectq.ops import All, Measure, QubitOperator, TimeEvolution
+from projectq.ops import All, Measure
 from projectq.meta import Control
 from projectq import MainEngine
 
-from projectq.backends import CircuitDrawer
+from projectq.backends import CircuitDrawer, CommandPrinter
 
 import cmath
 import numpy as np
 from projectq.ops import (BasicGate)
-from projectq.types import BasicQubit
 
 def phase_estimation(eng,unitary,eigenvector,n_ancillas):
 
@@ -112,58 +111,59 @@ class PhaseXxX(BasicGate):
 
 
 if __name__ == "__main__":
+
    #Create the compiler engine
 
-   drawing_engine = CircuitDrawer()
+#======== Testing ====   drawing_engine = CircuitDrawer()
+#======== Testing ====   print_engine = CommandPrinter()
    
-##   eng = MainEngine(drawing_engine)
+#======== Testing ====   eng = MainEngine(drawing_engine)
+
    eng = MainEngine()
    
-   ### Select an example an uncomment/comment as needed ###
+   ### Select an example and uncomment/comment as needed ###
 
-   ### Example ###unit = QubitOperator('X0 X1') 
-   ### Example ###unit = TimeEvolution(1.0,unitario)
-   ### Example ###autovector = eng.allocate_qureg(2)
+   #### X ###
 
-   ### Example ####unit = X
-   ### Example ####autovector = eng.allocate_qureg(1)
-
-   #### Defined phase with X:PhX ###
-
-   ####print("Example: Defined phase with PhX: Example |-> theta: .65625 (.15625) #ancillas:5")
+   ####print("Example: X: Example |-> theta: .5 #ancillas:5")
    ####autovector = eng.allocate_qureg(1)
    ####X | autovector
    ####H | autovector
-   ####theta = float(input ("Enter phase [0,1): "))
-   ####unit = PhaseX(theta)
+   ####unit = X
+
+   #### END X ###
+
+   #### Defined phase with X:PhX ###
+
+   print("Example: Defined phase with PhX: Example |-> theta: .65625 (.15625) #ancillas:5")
+   autovector = eng.allocate_qureg(1)
+   X | autovector
+   H | autovector
+   theta = float(input ("Enter phase [0,1): "))
+   unit = PhaseX(theta)
    
    #### END Defined phase with X:PhX ###
 
    #### Defined phase with PhX (x) X ###
 
-   print("Example: PhX (x) X: Example |->|-> theta: .65625 (.15625) #ancillas:5")
-   autovector = eng.allocate_qureg(2)
-   Tensor(X) | autovector
-   Tensor(H) | autovector
-   theta = float(input ("Enter phase [0,1): "))
-   unit = PhaseXxX(theta)
+   ####print("Example: PhX (x) X: Example |->|+> theta: .65625 (.15625) #ancillas:5")
+   ####autovector = eng.allocate_qureg(2)
+   ####X | autovector[0]
+   ####Tensor(H) | autovector
+   ####theta = float(input ("Enter phase [0,1): "))
+   ####unit = PhaseXxX(theta)
    
-   #### END Defined phase with X ###
-
-   ### Example ###X | autovector[1]
-   ### Example ####X | autovector[0]
-   ### Example ###All(H) | autovector
+   #### END Defined phase with PhX (x) X ###
 
    # Ask for the number of ancillas to use
    ene = int(input("How many ancillas?: "))
+
    # Call the phase_estimation function
    fase = phase_estimation(eng,unit,autovector,ene)
 
-#======== Testing ====
-
-   #unit | autovector
-   eng.flush()
-   print(drawing_engine.get_latex(),file=open("pe.tex", "w"))
+#======== Testing ====   eng.flush()
+   
+#======== Testing ====   print(drawing_engine.get_latex(),file=open("pe.tex", "w"))
 
 #======== Testing ====   amp_after1 = eng.backend.get_amplitude('00', autovector)
 #======== Testing ====   amp_after2 = eng.backend.get_amplitude('01', autovector)
@@ -175,7 +175,7 @@ if __name__ == "__main__":
 #======== Testing ====   print("Amplitude saved in amp_after3: {}".format(amp_after3))
 #======== Testing ====   print("Amplitude saved in amp_after4: {}".format(amp_after4))
 
-   # Deferred measure del estado para que pueda imprimir cosas
+   # Deferred measure of the state in order to be print
 
    All(Measure) | autovector
 
