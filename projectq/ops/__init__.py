0d1a78f3e147e3f69651261498d43730da08308b
Damian Steiger <damiansteiger@users.noreply.github.com>
2018-08-09 17:14:24 +0200

    Add state preparation (#254)

--- projectq/ops/__init__.py
+++ projectq/ops/__init__.py
@@ -35,3 +35,4 @@ from ._shortcuts import *
 from ._time_evolution import TimeEvolution
 from ._uniformly_controlled_rotation import (UniformlyControlledRy,
                                              UniformlyControlledRz)
+from ._state_prep import StatePreparation
