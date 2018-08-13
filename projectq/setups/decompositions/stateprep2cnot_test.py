622b8e517338d15323f04f5f69de77d310d7cff0
Damian S. Steiger <dsteiger@phys.ethz.ch>
2018-08-13 18:59:00 +0200

    Check for more user errors (normalized state) and extend collapse_wavefunction to take a string as input

--- projectq/setups/decompositions/stateprep2cnot_test.py
+++ projectq/setups/decompositions/stateprep2cnot_test.py
@@ -35,6 +35,9 @@ def test_wrong_final_state():
     cmd = Command(None, StatePreparation([0, 1j]), qubits=([qb0, qb1],))
     with pytest.raises(ValueError):
         stateprep2cnot._decompose_state_preparation(cmd)
+    cmd2 = Command(None, StatePreparation([0, 0.999j]), qubits=([qb0, qb1],))
+    with pytest.raises(ValueError):
+        stateprep2cnot._decompose_state_preparation(cmd2)
 
 
 @pytest.mark.parametrize("zeros", [True, False])
