92184ccc23e5080066a36f9f5a3a9846eecb459c
Thomas Haener <thomashaener@users.noreply.github.com>
2018-12-22 16:57:21 +0100

    Remove unused import.

--- projectq/ops/_state_prep.py
+++ projectq/ops/_state_prep.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from ._basics import BasicGate, SelfInverseGate
+from ._basics import BasicGate
 
 
 class StatePreparation(BasicGate):
