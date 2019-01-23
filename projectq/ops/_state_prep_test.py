3953b1f287049557c42ad5dc269416e135465db3
WrathfulSpatula <stranoj@gmail.com>
2019-01-23 01:21:01 -0500

    StatePreparation coverage

--- projectq/ops/_state_prep_test.py
+++ projectq/ops/_state_prep_test.py
@@ -16,7 +16,9 @@
 
 import projectq
 
-from projectq.ops import _state_prep, X
+from projectq.ops import _state_prep, X, StatePreparation
+from projectq import MainEngine
+from projectq.backends import Simulator
 
 
 def test_equality_and_hash():
@@ -32,3 +34,14 @@ def test_equality_and_hash():
 def test_str():
     gate1 = _state_prep.StatePreparation([0, 1])
     assert str(gate1) == "StatePreparation"
+
+def test_engine():
+    backend = Simulator()
+    eng = MainEngine(backend=backend, engine_list=[])
+    gate = StatePreparation([0.0, 1.0])
+
+    if (eng.backend.is_available(gate)):
+        qubit = eng.allocate_qubit()
+        gate | qubit
+        assert 0 == test_sim.get_amplitude([0], qubit)
+        assert 1 == test_sim.get_amplitude([1], qubit)
