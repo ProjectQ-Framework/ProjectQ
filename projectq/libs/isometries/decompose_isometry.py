769fe736eb58654f6c46ef5d2c8a4917858df8d3
Emanuel Malvetti <maemanue@student.ethz.ch>
2019-02-19 11:05:01 +0100

    Fix python 2.7 compatibility

--- projectq/libs/isometries/decompose_isometry.py
+++ projectq/libs/isometries/decompose_isometry.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from projectq import MainEngine
 from projectq.ops import Measure, X, Rz, UniformlyControlledGate, DiagonalGate
 from projectq.ops._basics import BasicGate
