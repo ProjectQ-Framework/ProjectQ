02afbbf8f94cfc5625d9464edf0761436b0ccfd3
Fernando de la Iglesia <fernando.delaiglesia@gmail.com>
2019-07-17 21:12:09 +0200

    Address comments by Damien including eliminate the usage of algorith_inverse and eliminate QPE files form the PR

--- projectq/setups/decompositions/phaseestimation_test.py
+++ projectq/setups/decompositions/phaseestimation_test.py
@@ -112,7 +112,7 @@ def test_2qubitsPh_andfunction_eigenvectors():
         eng.flush()
 
     num_phase = (results == 0.125).sum()
-    assert num_phase/100. >= 0.34, "Statistics phase calculation are not correct (%f vs. %f)" % (num_phase/100., 0.34)
+    assert num_phase/100. >= 0.35, "Statistics phase calculation are not correct (%f vs. %f)" % (num_phase/100., 0.35)
 
 
 def test_X_no_eigenvectors():
