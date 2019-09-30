640ac99bd70666fe74442e5a8f899b7526b0e147
Damien Nguyen <damien1@huawei.com>
2019-09-30 11:05:38 +0200

    Rewrite setup.py to fix some issues on Mac OSX and some code cleanup
    - On Mac OSX with Homebrew, properly find the path to the OpenMP
      library if compiling with clang from Homebrew
    - Code cleanup and some reformating

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ class get_pybind_include(object):
     The purpose of this class is to postpone importing pybind11
     until it is actually installed, so that the ``get_include()``
     method can be invoked. """
-
     def __init__(self, user=False):
         self.user = user
 
@@ -44,46 +43,41 @@ cppsim = Feature(
                 get_pybind_include(),
                 get_pybind_include(user=True)
             ],
-            language='c++'
-        ),
+            language='c++'),
     ],
 )
 
 
-def has_flag(compiler, flagname=None):
+def compiler_test(compiler,
+                  flagname=None,
+                  link=False,
+                  include="",
+                  body="",
+                  postargs=None):
     """
     Return a boolean indicating whether a flag name is supported on the
     specified compiler.
     """
     import tempfile
     f = tempfile.NamedTemporaryFile('w', suffix='.cpp', delete=False)
-    f.write('int main (int argc, char **argv) { return 0; }')
+    f.write('{}\nint main (int argc, char **argv) {{ {} return 0; }}'.format(
+        include, body))
     f.close()
     ret = True
+
+    if postargs is None:
+        postargs = [flagname] if flagname is not None else None
+    elif flagname is not None:
+        postargs.append(flagname)
+
     try:
-        if flagname is None:
-            compiler.compile([f.name])
-        else:
-            compiler.compile([f.name], extra_postargs=[flagname])
+        exec_name = os.path.join(tempfile.mkdtemp(), 'test')
+        obj_file = compiler.compile([f.name], extra_postargs=postargs)
+        if link:
+            compiler.link_executable(obj_file,
+                                     exec_name,
+                                     extra_postargs=postargs)
     except:
-        ret = False
-    os.unlink(f.name)
-    return ret
-
-
-def knows_intrinsics(compiler):
-    """
-    Return a boolean indicating whether the compiler can handle intrinsics.
-    """
-    import tempfile
-    f = tempfile.NamedTemporaryFile('w', suffix='.cpp', delete=False)
-    f.write('#include <immintrin.h>\nint main (int argc, char **argv) '
-            '{ __m256d neg = _mm256_set1_pd(1.0); }')
-    f.close()
-    ret = True
-    try:
-        compiler.compile([f.name], extra_postargs=['-march=native'])
-    except setuptools.distutils.errors.CompileError:
         ret = False
     os.unlink(f.name)
     return ret
@@ -99,26 +93,65 @@ class BuildExt(build_ext):
     def build_extensions(self):
         if sys.platform == 'darwin':
             self.c_opts['unix'] += ['-mmacosx-version-min=10.7']
-            if has_flag(self.compiler, '-stdlib=libc++'):
+            if compiler_test(self.compiler, '-stdlib=libc++'):
                 self.c_opts['unix'] += ['-stdlib=libc++']
 
         ct = self.compiler.compiler_type
         opts = self.c_opts.get(ct, [])
+        link_opts = []
 
-        if not has_flag(self.compiler):
+        if not compiler_test(self.compiler):
             self.warning("Something is wrong with your C++ compiler.\n"
                          "Failed to compile a simple test program!\n")
             return
 
+        # ------------------------------
+        # Test for OpenMP
+
+        kwargs = {
+            'link': True,
+            'include': '#include <omp.h>',
+            'body': 'int a = omp_get_num_threads(); ++a;'
+        }
         openmp = ''
-        if has_flag(self.compiler, '-fopenmp'):
+        if compiler_test(self.compiler, '-fopenmp', **kwargs):
             openmp = '-fopenmp'
-        elif has_flag(self.compiler, '-qopenmp'):
+        elif compiler_test(self.compiler, '-qopenmp', **kwargs):
             openmp = '-qopenmp'
+        elif (sys.platform == 'darwin'
+              and compiler_test(self.compiler, '-fopenmp')):
+            try:
+                llvm_root = subprocess.check_output(
+                    ['brew', '--prefix', 'llvm']).decode('utf-8')[:-1]
+                compiler_root = subprocess.check_output(
+                    ['which', self.compiler.compiler[0]]).decode('utf-8')[:-1]
+
+                # Only add the flag if the compiler we are using is the one
+                # from HomeBrew
+                if llvm_root in compiler_root:
+                    l_arg = '-L{}/lib'.format(llvm_root)
+                if compiler_test(self.compiler,
+                                 '-fopenmp',
+                                 postargs=[l_arg],
+                                 **kwargs):
+                    link_opts.append(l_arg)
+                    openmp = '-fopenmp'
+            except subprocess.CalledProcessError:
+                pass
+
         if ct == 'msvc':
             openmp = ''  # supports only OpenMP 2.0
 
-        if knows_intrinsics(self.compiler):
+        opts.append(openmp)
+
+        # ------------------------------
+        # Test for compiler intrinsics
+
+        if compiler_test(self.compiler,
+                         flagname='-march=native',
+                         link=True,
+                         include='#include <immintrin.h>',
+                         body='__m256d neg = _mm256_set1_pd(1.0); (void)neg;'):
             opts.append('-DINTRIN')
             if ct == 'msvc':
                 opts.append('/arch:AVX')
@@ -126,23 +159,31 @@ class BuildExt(build_ext):
                 opts.append('-march=native')
                 opts.append('-ffast-math')
 
-        opts.append(openmp)
+        # ------------------------------
+        # Other compiler tests
+
         if ct == 'unix':
-            if not has_flag(self.compiler, '-std=c++11'):
+            # Avoiding C++17 for now because of compilation issues on MacOSX
+            if compiler_test(self.compiler, '-std=c++14'):
+                opts.append('-std=c++14')
+            elif compiler_test(self.compiler, '-std=c++11'):
+            opts.append('-std=c++11')
+            else:
                 self.warning("Compiler needs to have C++11 support!")
                 return
 
-            opts.append('-DVERSION_INFO="%s"'
-                        % self.distribution.get_version())
-            opts.append('-std=c++11')
-            if has_flag(self.compiler, '-fvisibility=hidden'):
+            opts.append('-DVERSION_INFO="%s"' %
+                        self.distribution.get_version())
+            if compiler_test(self.compiler, '-fvisibility=hidden'):
                 opts.append('-fvisibility=hidden')
         elif ct == 'msvc':
-            opts.append('/DVERSION_INFO=\\"%s\\"'
-                        % self.distribution.get_version())
+            opts.append('/DVERSION_INFO=\\"%s\\"' %
+                        self.distribution.get_version())
+
+        link_opts.append(openmp)
         for ext in self.extensions:
             ext.extra_compile_args = opts
-            ext.extra_link_args = [openmp]
+            ext.extra_link_args = link_opts
         try:
             build_ext.build_extensions(self)
         except setuptools.distutils.errors.CompileError:
@@ -155,8 +196,7 @@ class BuildExt(build_ext):
                         "building the (faster) C++ version of the simulator.")
 
 
-setup(
-    name='projectq',
+setup(name='projectq',
     version=__version__,
     author='ProjectQ',
     author_email='info@projectq.ch',
@@ -169,5 +209,4 @@ setup(
     cmdclass={'build_ext': BuildExt},
     zip_safe=False,
     license='Apache 2',
-    packages=find_packages()
-)
+      packages=find_packages())
