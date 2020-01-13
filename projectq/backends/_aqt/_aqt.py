22615ada004fb450fe4882843473d1c647f18dfd
dbretaud <logiciqt@gmail.com>
2020-01-13 23:46:28 +0000

    Backend implementation for aqt trapped ion quantum computer.

---
+++ projectq/backends/_aqt/_aqt.py
@@ -0,0 +1,310 @@
+#   Copyright 2017 ProjectQ-Framework (www.projectq.ch)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+""" Back-end to run quantum program on AQT's API."""
+import math
+import random
+import json
+
+from projectq.cengines import BasicEngine
+from projectq.meta import get_control_count, LogicalQubitIDTag
+from projectq.ops import (Rx,
+                          Ry,
+                          Rxx,
+                          Measure,
+                          Allocate,
+                          Barrier,
+                          Deallocate,
+                          FlushGate)
+
+from ._aqt_http_client import send, retrieve
+
+
+class AQTBackend(BasicEngine):
+    """
+    The AQT Backend class, which stores the circuit, transforms it to the appropriate data format,
+    and sends the circuit through the AQT API.
+    """
+    def __init__(self, use_hardware=False, num_runs=100, verbose=False,
+                 token='', device='simulator',
+                 num_retries=3000, interval=1,
+                 retrieve_execution=None):
+        """
+        Initialize the Backend object.
+
+        Args:
+            use_hardware (bool): If True, the code is run on the IBM quantum
+                chip (instead of using the IBM simulator)
+            num_runs (int): Number of runs to collect statistics.
+                (default is 100, max is usually around 250)
+            verbose (bool): If True, statistics are printed, in addition to
+                the measurement result being registered (at the end of the
+                circuit).
+            token (str): AQT user password. 
+            device (str): name of the AQT device to use. simulator By default
+            num_retries (int): Number of times to retry to obtain
+                results from the AQT API. (default is 3000)
+            interval (float, int): Number of seconds between successive
+                attempts to obtain results from the AQT API.
+                (default is 1)
+            retrieve_execution (int): Job ID to retrieve instead of re-
+                running the circuit (e.g., if previous run timed out).
+        """
+        BasicEngine.__init__(self)
+        self._reset()
+        if use_hardware:
+            self.device = device
+        else:
+            self.device = 'simulator'
+        self._num_runs = num_runs
+        self._verbose = verbose
+        self._token=token
+        self._num_retries = num_retries
+        self._interval = interval
+        self._probabilities = dict()
+        self._circuit=[]
+        self._measured_ids = []
+        self._allocated_qubits = set()
+        self._retrieve_execution = retrieve_execution
+
+    def is_available(self, cmd):
+        """
+        Return true if the command can be executed.
+
+        The AQT ion trap can only do Rx,Ry and Rxx.
+
+        Args:
+            cmd (Command): Command for which to check availability
+        """
+        g = cmd.gate
+        if get_control_count(cmd) == 0:
+            if isinstance(g, (Rx, Ry, Rxx)):
+                return True
+        if g in (Measure, Allocate, Deallocate,Barrier):
+            return True
+        return False
+
+    def _reset(self):
+        """ Reset all temporary variables (after flush gate). """
+        self._clear = True
+        self._measured_ids = []
+
+    def _store(self, cmd):
+        """
+        Temporarily store the command cmd.
+
+        Translates the command and stores it in a local variable (self._cmds).
+
+        Args:
+            cmd: Command to store
+        """
+        if self._clear:
+            self._probabilities = dict()
+            self._clear = False
+            self._circuit=[]
+            self._allocated_qubits = set()
+
+        gate = cmd.gate
+        if gate == Allocate:
+            self._allocated_qubits.add(cmd.qubits[0][0].id)
+            return
+        if gate == Deallocate:
+            return
+        if gate == Measure:
+            assert len(cmd.qubits) == 1 and len(cmd.qubits[0]) == 1
+            qb_id = cmd.qubits[0][0].id
+            logical_id = None
+            for t in cmd.tags:
+                if isinstance(t, LogicalQubitIDTag):
+                    logical_id = t.logical_qubit_id
+                    break
+            assert logical_id is not None
+            self._measured_ids += [logical_id]
+            return
+        if isinstance(gate, (Rx, Ry, Rxx)):
+            qubits=[]
+            qubits.append(cmd.qubits[0][0].id)
+            if len(cmd.qubits) == 2:
+                qubits.append(cmd.qubits[1][0].id)
+            angle=gate.angle/math.pi
+            instruction=[]
+            u_name = {'Rx': "X", 'Ry': "Y",
+                      'Rxx': "MS"}
+            instruction.append(u_name[str(gate)[0:int(len(cmd.qubits)+1)]])
+            instruction.append(round(angle,2))
+            instruction.append(qubits)
+            self._circuit.append(instruction)
+            return
+        if gate == Barrier:
+            return
+        else:
+            raise Exception('Invalid command: '+str(cmd))
+            pass
+
+    def _logical_to_physical(self, qb_id):
+        """
+        Return the physical location of the qubit with the given logical id.
+
+        Args:
+            qb_id (int): ID of the logical qubit whose position should be
+                returned.
+        """
+        assert self.main_engine.mapper is not None
+        mapping = self.main_engine.mapper.current_mapping
+        if qb_id not in mapping:
+            raise RuntimeError("Unknown qubit id {}. Please make sure "
+                               "eng.flush() was called and that the qubit "
+                               "was eliminated during optimization."
+                               .format(qb_id))
+        return mapping[qb_id]
+
+    def get_probabilities(self, qureg):
+        """
+        Return the list of basis states with corresponding probabilities.
+
+        The measured bits are ordered according to the supplied quantum
+        register, i.e., the left-most bit in the state-string corresponds to
+        the first qubit in the supplied quantum register.
+
+        Warning:
+            Only call this function after the circuit has been executed!
+
+        Args:
+            qureg (list<Qubit>): Quantum register determining the order of the
+                qubits.
+
+        Returns:
+            probability_dict (dict): Dictionary mapping n-bit strings to
+            probabilities.
+
+        Raises:
+            RuntimeError: If no data is available (i.e., if the circuit has
+                not been executed). Or if a qubit was supplied which was not
+                present in the circuit (might have gotten optimized away).
+        """
+        if len(self._probabilities) == 0:
+            raise RuntimeError("Please, run the circuit first!")
+
+        probability_dict = dict()
+
+        for state in self._probabilities:
+            mapped_state = ['0'] * len(qureg)
+            for i in range(len(qureg)):
+                mapped_state[i] = state[self._logical_to_physical(qureg[i].id)]
+            probability = self._probabilities[state]
+            probability_dict["".join(mapped_state)] = probability
+
+        return probability_dict
+
+    #_rearrange_result & _format_counts imported and modified from qiskit
+    def _rearrange_result(self, input,length):
+        bin_input = list(bin(input)[2:].rjust(length, '0'))
+        return ''.join(bin_input)
+
+    def _format_counts(self, samples,length):
+        counts = {}
+        for result in samples:
+            h_result = self._rearrange_result(result,length)
+            if h_result not in counts:
+                counts[h_result] = 1
+            else:
+                counts[h_result] += 1
+        counts={k: v for k, v in sorted(counts.items(), key=lambda item: item[0])}
+        return counts
+        
+    def _run(self):
+        """
+        Run the circuit.
+
+        Send the circuit via the AQT API using the provided user
+        token / ask for the user token.
+        """
+        # finally: measurements 
+        # NOTE AQT DOESN'T SEEM TO HAVE MEASUREMENT INSTRUCTIONS (no intermediate measurements are allowed, so implicit at the end)
+        # return if no operations.
+        if self._circuit == []:
+            return
+
+        max_qubit_id = max(self._allocated_qubits)
+        info = {}
+        # Hack: AQT instructions specifically need "GATE" string representation
+        # instead of 'GATE' 
+        info['circuit']=str(self._circuit).replace("'",'"')
+        info['nq']=max_qubit_id + 1
+
+        info['shots'] = self._num_runs
+        info['backend'] = {'name': self.device}
+        if self._num_runs>200:
+            raise Exception("Number of shots limited to 200")
+        try:
+            if self._retrieve_execution is None:
+                res = send(info, device=self.device,
+                           token=self._token,
+                           shots=self._num_runs,
+                           num_retries=self._num_retries,
+                           interval=self._interval,
+                           verbose=self._verbose)
+            else:
+                res = retrieve(device=self.device, 
+                               token=self._token,
+                               jobid=self._retrieve_execution,
+                               num_retries=self._num_retries,
+                               interval=self._interval,
+                               verbose=self._verbose)
+            length=len(self._measured_ids)
+            counts = self._format_counts(res,length)
+            # Determine random outcome
+            P = random.random()
+            print(P)
+            p_sum = 0.
+            measured = ""
+            for state in counts:
+                probability = counts[state] * 1. / self._num_runs
+                p_sum += probability
+                star = ""
+                if p_sum >= P and measured == "":
+                    measured = state
+                    star = "*"
+                self._probabilities[state] = probability
+                if self._verbose and probability > 0:
+                    print(str(state) + " with p = " + str(probability) +
+                          star)
+
+            class QB():
+                def __init__(self, ID):
+                    self.id = ID
+
+            # register measurement result
+            for ID in self._measured_ids:
+                location = self._logical_to_physical(ID)
+                result = int(measured[location])
+                self.main_engine.set_measurement_result(QB(ID), result)
+            self._reset()
+        except TypeError:
+            raise Exception("Failed to run the circuit. Aborting.")
+
+    def receive(self, command_list):
+        """
+        Receives a command list and, for each command, stores it until
+        completion.
+
+        Args:
+            command_list: List of commands to execute
+        """
+        for cmd in command_list:
+            if not cmd.gate == FlushGate():
+                self._store(cmd)
+            else:
+                self._run()
+                self._reset()
---
+++ projectq/backends/_aqt/_aqt_http_client.py
@@ -0,0 +1,230 @@
+#   Copyright 2017 ProjectQ-Framework (www.projectq.ch)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import requests
+import getpass
+import json
+import signal
+import sys
+import time
+from requests.compat import urljoin
+from requests import Session
+
+#an AQT token can be requested at:
+#https://gateway-portal.aqt.eu/
+
+_api_url = 'https://gateway.aqt.eu/marmot/'
+
+class AQT(Session):
+    def __init__(self):
+        super().__init__()
+        self.backends=dict()
+        self.timeout=5.0
+        self.token=None
+
+    def get_list_devices(self,verbose=False):
+        """
+        return list of available devices
+
+        Up to my knowledge there is no proper API call for online devices,
+        so we just assume that the list from AQT portal always up to date
+        """
+        self.backends=dict()
+        self.backends['aqt_simulator']={'nq': 11,'version':'0.0.1','url':'sim/'}
+        self.backends['aqt_simulator_noise']={'nq': 11,'version':'0.0.1','url':'sim/noise-model-1'}
+        self.backends['aqt_device']={'nq': 4,'version':'0.0.1','url':'lint/'}
+        if verbose==True:
+            print('- List of AQT devices available:')
+            print(self.backends)
+        return self.backends
+
+    def is_online(self,device):#useless at the moment, may change if API is evolving
+        return device in self.backends
+
+    def can_run_experiment(self,info,device):
+        """
+        check if the device is big enough to run the code
+
+        Args:
+            info (dict): dictionary sent by the backend containing the code to run
+            device (str): name of the aqt device to use
+        :return:
+            (bool): True if device is big enough, False otherwise
+        """
+        nb_qubit_max=self.backends[device]['nq']
+        nb_qubit_needed=info['nq']
+        return nb_qubit_needed<=nb_qubit_max,nb_qubit_max,nb_qubit_needed
+
+    def _authenticate(self,token=None):
+        """
+        :param token:
+        :return:
+        """
+        if token is None:
+            token = getpass.getpass(prompt='AQT Q token > ')
+        self.headers.update({'Ocp-Apim-Subscription-Key': token, 'SDK': 'ProjectQ'})
+        self.token=token
+        return 
+
+    def _run(self,info, device):
+        shots=info['shots']
+        nq=info['nq']
+        instructions=info['circuit']
+        argument={'data': instructions, 'access_token':self.token,'repetitions':shots,'no_qubits':nq}
+        r = super().put(urljoin(_api_url, self.backends[device]['url']), data=argument)
+        r.raise_for_status()
+        r_json=r.json()
+        if r_json['status'] != 'queued':
+            raise Exception('Error in sending the code online')
+        execution_id = r_json["id"]
+        return execution_id
+
+    def _get_result(self,device, execution_id, num_retries=3000,
+                    interval=1, verbose=False):
+
+        if verbose:
+            print("Waiting for results. [Job ID: {}]".format(execution_id))
+
+        original_sigint_handler = signal.getsignal(signal.SIGINT)
+
+        def _handle_sigint_during_get_result(*_):
+            raise Exception("Interrupted. The ID of your submitted job is {}."
+                            .format(execution_id))
+
+        try:
+            signal.signal(signal.SIGINT, _handle_sigint_during_get_result)
+
+            for retries in range(num_retries):
+                
+                argument={'id': execution_id, 'access_token': self.token}
+                r = super().put(urljoin(_api_url, self.backends[device]['url']), data=argument)
+                r.raise_for_status()
+                r_json = r.json()
+                if r_json['status']=='finished':
+                    return r_json['samples']
+                elif r_json['status']!='running':
+                    raise Exception("Error while running the code: {}."
+                        .format(r_json['status']))
+                time.sleep(interval)
+                if self.is_online(device) and retries % 60 == 0:
+                    self.get_list_devices()
+                    if not self.is_online(device):
+                        raise DeviceOfflineError("Device went offline. The ID of "
+                                                "your submitted job is {}."
+                                                .format(execution_id))
+
+        finally:
+            if original_sigint_handler is not None:
+                signal.signal(signal.SIGINT, original_sigint_handler)
+
+        raise Exception("Timeout. The ID of your submitted job is {}."
+                        .format(execution_id))
+
+class DeviceTooSmall(Exception):
+    pass
+
+class DeviceOfflineError(Exception):
+    pass
+
+def show_devices(verbose=False):    
+    """
+    Access the list of available devices and their properties (ex: for setup configuration)
+
+    Args:
+        verbose (bool): If True, additional information is printed
+
+    Return:
+        (list) list of available devices and their properties
+    """
+    aqt_session=AQT()
+    return aqt_session.get_list_devices(verbose=verbose)
+
+def retrieve(device, token, jobid, num_retries=3000,
+             interval=1, verbose=False):
+    """
+    Retrieves a previously run job by its ID.
+
+    Args:
+        device (str): Device on which the code was run / is running.
+        token (str): AQT quantum experience user token.
+        jobid (str): Id of the job to retrieve
+    
+    Return:
+        (list) samples form the AQT server
+    """
+    aqt_session=AQT()
+    aqt_session._authenticate(token)
+    res = aqt_session._get_result(device, jobid, num_retries=num_retries,
+                      interval=interval, verbose=verbose)
+    return res
+
+
+def send(info, device='aqt_simulator',token=None,
+         shots=100, num_retries=100, interval=1, verbose=False):
+    """
+    Sends cicruit through the AQT API and runs the quantum circuit.
+
+    Args:
+        info(dict): Contains representation of the circuit to run.
+        device (str): name of the aqt device. Simulator chosen by default
+        token (str): AQT quantum experience user token.
+        shots (int): Number of runs of the same circuit to collect statistics. max for AQT is 200.
+        verbose (bool): If True, additional information is printed, such as
+            measurement statistics. Otherwise, the backend simply registers
+            one measurement result (same behavior as the projectq Simulator).
+
+    Return:
+        (list) samples form the AQT server
+
+    """
+    try:
+        aqt_session=AQT()
+
+        if verbose:
+            print("- Authenticating...")
+            print('TOKEN: '+token)
+        aqt_session._authenticate(token)
+
+        # check if the device is online
+        aqt_session.get_list_devices(verbose)
+        online = aqt_session.is_online(device)
+        if not online: #useless for the moment
+            print("The device is offline (for maintenance?). Use the "
+                  "simulator instead or try again later.")
+            raise DeviceOfflineError("Device is offline.")
+            
+        # check if the device has enough qubit to run the code
+        runnable,qmax,qneeded = aqt_session.can_run_experiment(info,device)
+        if not runnable:
+            print("The device is too small ({} qubits available) for the code requested({} qubits needed"
+                    "Try to look for another device with more qubits".format(qmax,qneeded))
+            raise DeviceTooSmall("Device is too small.")
+        if verbose:
+            print("- Running code: {}".format(info))
+        execution_id = aqt_session._run(info, device)
+        if verbose:
+            print("- Waiting for results...")
+        res = aqt_session._get_result(device, execution_id,num_retries=num_retries,interval=interval, verbose=verbose)
+        if verbose:
+            print("- Done.")
+        return res
+    except requests.exceptions.HTTPError as err:
+        print("- There was an error running your code:")
+        print(err)
+    except requests.exceptions.RequestException as err:
+        print("- Looks like something is wrong with server:")
+        print(err)
+    except KeyError as err:
+        print("- Failed to parse response:")
+        print(err)
