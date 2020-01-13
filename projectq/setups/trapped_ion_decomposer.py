7e30ed57d1d99852bd407ecdfa140693553c392b
Damien Nguyen <damien1@huawei.com>
2020-01-13 14:15:09 +0100

    Fix a few issues with the trapped-ion setup
    - Store the sign of the last Ry gate on an engine-by-engine basis
    - Cleanup of some remaining print statements
    - Some stylistic issues fixed

--- projectq/setups/trapped_ion_decomposer.py
+++ projectq/setups/trapped_ion_decomposer.py
@@ -12,149 +12,137 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
-#   Module uses ideas from “Basic circuit compilation techniques 
-#   for an ion-trap quantum machine” by Dmitri Maslov (2017) at 
+#   Module uses ideas from “Basic circuit compilation techniques
+#   for an ion-trap quantum machine” by Dmitri Maslov (2017) at
 #   https://iopscience.iop.org/article/10.1088/1367-2630/aa5e47
-
 """
 Apply the restricted gate set setup for trapped ion based quantum computers.
 
 It provides the `engine_list` for the `MainEngine`, restricting the gate set to
 Rx and Ry single qubit gates and the Rxx two qubit gates.
 
-A decomposition chooser is implemented following the ideas in QUOTE for reducing the
-number of Ry gates in the new circuit. 
-NOTE: Because the decomposition chooser is only called when a gate has to be decomposed,
-this reduction will work better when the entire circuit has to be decomposed. Otherwise,
-If the circuit has both superconding gates and native ion trapped gates the decomposed
-circuit will not be optimal.  
+A decomposition chooser is implemented following the ideas in QUOTE for
+reducing the number of Ry gates in the new circuit.
+
+NOTE:
+
+Because the decomposition chooser is only called when a gate has to be
+decomposed, this reduction will work better when the entire circuit has to be
+decomposed. Otherwise, If the circuit has both superconding gates and native
+ion trapped gates the decomposed circuit will not be optimal.
 """
 
-import inspect
-
-import projectq
-import projectq.libs.math
 from projectq.setups import restrictedgateset
-from projectq.cengines import (AutoReplacer, DecompositionRuleSet,
-                               InstructionFilter, LocalOptimizer,
-                               TagRemover)
-from projectq.ops import (Rxx,Rx,Ry)
+from projectq.ops import (Rxx, Rx, Ry)
+from projectq.meta import get_control_count
 
 # ------------------chooser_Ry_reducer-------------------#
-# If the qubit is not in the prev_Ry_sign dictionary, then no decomposition occured
-# If the value is -1 then the last gate applied (during a decomposition!) was Ry(-math.pi/2)
-# If the value is 1 then the last gate applied (during a decomposition!) was Ry(math.pi/2)
-# If the value is 0 then the last gate applied (during a decomposition!) was a Rx
+# If the qubit is not in the prev_Ry_sign dictionary, then no decomposition
+# occured
+# If the value is:
+#  -1 then the last gate applied (during a decomposition!) was Ry(-math.pi/2)
+#   1 then the last gate applied (during a decomposition!) was Ry(+math.pi/2)
+#   0 then the last gate applied (during a decomposition!) was a Rx
 
-prev_Ry_sign=dict() # Keeps track of most recent Ry sign, i.e.
-                    # whether we had Ry(-pi/2) or Ry(pi/2)
-                    # prev_Ry_sign[qubit_index]  
-                    # should hold -1 or +1
+prev_Ry_sign = dict()  # Keeps track of most recent Ry sign, i.e.
+#                        whether we had Ry(-pi/2) or Ry(pi/2)
+#                        prev_Ry_sign[qubit_index] should hold -1 or
+#                        +1
 
-def chooser_Ry_reducer(cmd,decomposition_list):
+
+def chooser_Ry_reducer(cmd, decomposition_list):
     """
-    Choose the decomposition so as to maximise Ry cancellations,
-    based on the previous decomposition used for the given qubit. 
+    Choose the decomposition so as to maximise Ry cancellations, based on the
+    previous decomposition used for the given qubit.
 
     Note:
-        Classical instructions gates e.g. Flush and 
-        Measure are automatically allowed.
+        Classical instructions gates e.g. Flush and Measure are automatically
+        allowed.
 
     Returns:
         A decomposition object from the decomposition_list.
     """
-    decomp_rule=dict() 
-    name='default'
-    
-    for el in decomposition_list:
+    decomp_rule = dict()
+    name = 'default'
+
+    for decomp in decomposition_list:
         try:
-            decomposition=el.decompose.__name__.split('_')
-            name=decomposition[2]
-            decomp_rule[decomposition[3]]=el
-            # 'M' stands for minus, 'P' stands for plus
-            # 'N' stands for neutral
-            # e.g. decomp_rule['M'] will give you the 
-            # decomposition_rule that ends with a Ry(-pi/2)
+            # NB: need to (possibly) raise an exception before setting the
+            # name variable below
+            decomposition = decomp.decompose.__name__.split('_')
+            decomp_rule[decomposition[3]] = decomp
+            name = decomposition[2]
+            # 'M' stands for minus, 'P' stands for plus 'N' stands for neutral
+            # e.g. decomp_rule['M'] will give you the decomposition_rule that
+            # ends with a Ry(-pi/2)
         except IndexError:
             pass
-    if name=='cnot2rxx': 
-        idx = [qb.id for qb in cmd.control_qubits] # index of the qubit
-        assert len(idx)==1 # this should be a 2 qubit gate, therefore with 1 control qubit
-        idx=idx[0] 
-        print(idx)
-        if idx in prev_Ry_sign:
-            if prev_Ry_sign[idx]<=0:
-                # If the previous qubit had Ry(-pi/2)
-                # choose the decomposition that starts 
-                # with Ry(pi/2)
-                prev_Ry_sign[idx]=-1
-                # Now the prev_Ry_sign is set to -1
-                # since at the end of the decomposition
-                # we will have a Ry(-pi/2)
-                return decomp_rule['M']
-            else:
-                # Previous qubit had Ry(pi/2)
-                # choose decomposition that starts 
-                # with Ry(-pi/2) and ends with R(pi/2)
-                prev_Ry_sign[idx]=1
-                return decomp_rule['P']
-        else:
-            # In this case where we have no previous Ry sign 
-            # we choose decomp_rule['M']
-            prev_Ry_sign[idx]=-1
-            return decomp_rule['M']
-    elif name=='h2rx':
-        # Block operates similar to 'cnot2rxx' case
-        idx = [qb.id for qb in cmd.qubits[0]]
-        assert len(idx)==1 # this should be a single qubit gate
-        idx=idx[0]
-        if idx not in prev_Ry_sign:
-            prev_Ry_sign[idx]=+1
-            return decomp_rule['M']
-        elif prev_Ry_sign[idx]==0:
-            prev_Ry_sign[idx]=+1
-            return decomp_rule['M']
-        else:
-            prev_Ry_sign[idx]=00
-            return decomp_rule['N']
-    elif name=='rz2rx':
-        # Block operates similar to 'cnot2rxx' case
-        idx = [qb.id for qb in cmd.qubits[0]]
-        assert len(idx)==1 # this should be a single qubit gate
-        idx=idx[0]
-        if idx not in prev_Ry_sign:
-            prev_Ry_sign[idx]=-1
-            return decomp_rule['M']
-        elif prev_Ry_sign[idx]<=0:
-            prev_Ry_sign[idx]=-1
-            return decomp_rule['M']
-        else:
-            prev_Ry_sign[idx]=1
-            return decomp_rule['P']
-    else:   # No decomposition chosen, so use the first decompostion 
-            # in the list like the default function
-        return decomposition_list[0]
 
+    local_prev_Ry_sign = prev_Ry_sign.setdefault(cmd.engine, dict())
+
+    if name == 'cnot2rxx':
+        assert get_control_count(cmd) == 1
+        ctrl_id = cmd.control_qubits[0].id
+
+        if local_prev_Ry_sign.get(ctrl_id, -1) <= 0:
+            # If the previous qubit had Ry(-pi/2) choose the decomposition
+            # that starts with Ry(pi/2)
+            local_prev_Ry_sign[ctrl_id] = -1
+            # Now the prev_Ry_sign is set to -1 since at the end of the
+            # decomposition we will have a Ry(-pi/2)
+            return decomp_rule['M']
+
+        # Previous qubit had Ry(pi/2) choose decomposition that starts
+        # with Ry(-pi/2) and ends with R(pi/2)
+        local_prev_Ry_sign[ctrl_id] = 1
+        return decomp_rule['P']
+
+    if name == 'h2rx':
+        qubit_id = [qb.id for qureg in cmd.qubits for qb in qureg]
+        assert len(qubit_id) == 1  # this should be a single qubit gate
+        qubit_id = qubit_id[0]
+
+        if local_prev_Ry_sign.get(qubit_id, 0) == 0:
+            local_prev_Ry_sign[qubit_id] = 1
+            return decomp_rule['M']
+
+        local_prev_Ry_sign[qubit_id] = 0
+        return decomp_rule['N']
+
+    if name == 'rz2rx':
+        qubit_id = [qb.id for qureg in cmd.qubits for qb in qureg]
+        assert len(qubit_id) == 1  # this should be a single qubit gate
+        qubit_id = qubit_id[0]
+
+        if local_prev_Ry_sign.get(qubit_id, -1) <= 0:
+            local_prev_Ry_sign[qubit_id] = -1
+            return decomp_rule['M']
+
+        local_prev_Ry_sign[qubit_id] = 1
+        return decomp_rule['P']
+
+    # No decomposition chosen, so use the first decompostion in the list
+    # like the default function
+    return decomposition_list[0]
 
 
 def get_engine_list():
     """
-    Returns an engine list compiling code into a trapped ion based compiled circuit code.
+    Returns an engine list compiling code into a trapped ion based compiled
+    circuit code.
 
     Note:
-        -Classical instructions gates such as e.g. Flush and Measure are
+
+        - Classical instructions gates such as e.g. Flush and Measure are
         automatically allowed.
-        -The restricted gate set engine does not work with Rxx gates, as 
-        ProjectQ will by default bounce back and forth between Cz gates 
-        and Cx gates. An appropriate decomposition chooser needs to be used!
+        - The restricted gate set engine does not work with Rxx gates, as
+        ProjectQ will by default bounce back and forth between Cz gates and Cx
+        gates. An appropriate decomposition chooser needs to be used!
 
     Returns:
         A list of suitable compiler engines.
     """
-    return restrictedgateset.get_engine_list(one_qubit_gates=(Rx,Ry),
-            two_qubit_gates=(Rxx,),compiler_chooser=chooser_Ry_reducer)
-
-
-
-
- 
+    return restrictedgateset.get_engine_list(
+        one_qubit_gates=(Rx, Ry),
+        two_qubit_gates=(Rxx, ),
+        compiler_chooser=chooser_Ry_reducer)
--- projectq/setups/trapped_ion_decomposer_test.py
+++ projectq/setups/trapped_ion_decomposer_test.py
@@ -14,49 +14,139 @@
 
 "Tests for projectq.setups.trapped_ion_decomposer.py."
 
-import math
 import projectq
-from projectq import MainEngine
-from projectq.ops import Rx, Ry, Rz, H, CNOT, Measure, All, X, Rxx
-from projectq.meta import Compute, Control, Uncompute
-from projectq.cengines import DecompositionRule
-from projectq.cengines import (AutoReplacer, DecompositionRuleSet, DummyEngine,
-                               InstructionFilter)
-import pytest
-import projectq.setups.restrictedgateset as restrictedgateset
-from projectq.setups.trapped_ion_decomposer import chooser_Ry_reducer
+from projectq.ops import (Rx, Ry, Rz, H, X, CNOT, Measure, Rxx,
+                          ClassicalInstructionGate)
+from projectq.cengines import (MainEngine, DummyEngine, AutoReplacer,
+                               TagRemover, InstructionFilter,
+                               DecompositionRuleSet, DecompositionRule)
+from projectq.meta import get_control_count
 
-# Without the chooser_Ry_reducer function, i.e. if 
-# the restricted gate set just picked the first option in
-# each decomposition list, the circuit below would be
-# decomposed into 8 single qubit gates and 1 two qubit gate.
-# Including the Allocate and Measure commands, this would 
-# result in 12 commands. 
-# Using the chooser_Rx_reducer you get 9 commands, since you 
-# now have 4 single qubit gates and 1 two qubit gate.
+from . import restrictedgateset
+from .trapped_ion_decomposer import chooser_Ry_reducer
+
+
+def filter_gates(eng, cmd):
+    if isinstance(cmd.gate, ClassicalInstructionGate):
+        return True
+    if ((cmd.gate == X and get_control_count(cmd) == 1) or cmd.gate == H
+            or isinstance(cmd.gate, Rz)):
+        return False
+    return True
+
+
+def test_chooser_Ry_reducer_synthetic():
+    backend = DummyEngine(save_commands=True)
+    rule_set = DecompositionRuleSet(
+        modules=[projectq.libs.math, projectq.setups.decompositions])
+
+    engine_list = [
+        AutoReplacer(rule_set, chooser_Ry_reducer),
+        TagRemover(),
+        InstructionFilter(filter_gates),
+    ]
+
+    eng = MainEngine(backend=backend, engine_list=engine_list)
+    control = eng.allocate_qubit()
+    target = eng.allocate_qubit()
+    CNOT | (control, target)
+    CNOT | (control, target)
+    eng.flush()
+    idx0 = len(backend.received_commands) - 2
+    idx1 = len(backend.received_commands)
+    CNOT | (control, target)
+    eng.flush()
+
+    assert isinstance(backend.received_commands[idx0].gate, Ry)
+    assert isinstance(backend.received_commands[idx1].gate, Ry)
+    assert (backend.received_commands[idx0].gate.get_inverse() ==
+            backend.received_commands[idx1].gate)
+
+    eng = MainEngine(backend=backend, engine_list=engine_list)
+    control = eng.allocate_qubit()
+    target = eng.allocate_qubit()
+    H | target
+    eng.flush()
+    idx0 = len(backend.received_commands) - 2
+    idx1 = len(backend.received_commands)
+    H | target
+    eng.flush()
+
+    assert isinstance(backend.received_commands[idx0].gate, Ry)
+    assert isinstance(backend.received_commands[idx1].gate, Ry)
+    assert (backend.received_commands[idx0].gate.get_inverse() ==
+            backend.received_commands[idx1].gate)
+
+    eng = MainEngine(backend=backend, engine_list=engine_list)
+    control = eng.allocate_qubit()
+    target = eng.allocate_qubit()
+    Rz(1.23456) | target
+    eng.flush()
+    idx0 = len(backend.received_commands) - 2
+    idx1 = len(backend.received_commands)
+    Rz(1.23456) | target
+    eng.flush()
+
+    assert isinstance(backend.received_commands[idx0].gate, Ry)
+    assert isinstance(backend.received_commands[idx1].gate, Ry)
+    assert (backend.received_commands[idx0].gate.get_inverse() ==
+            backend.received_commands[idx1].gate)
+
+
+def _dummy_h2nothing_A(cmd):
+    qubit = cmd.qubits[0]
+    Ry(1.23456) | qubit
+
+
+def test_chooser_Ry_reducer_unsupported_gate():
+    backend = DummyEngine(save_commands=True)
+    rule_set = DecompositionRuleSet(
+        rules=[DecompositionRule(H.__class__, _dummy_h2nothing_A)])
+
+    engine_list = [
+        AutoReplacer(rule_set, chooser_Ry_reducer),
+        TagRemover(),
+        InstructionFilter(filter_gates),
+    ]
+
+    eng = MainEngine(backend=backend, engine_list=engine_list)
+    qubit = eng.allocate_qubit()
+    H | qubit
+    eng.flush()
+
+    for cmd in backend.received_commands:
+        print(cmd)
+
+    assert isinstance(backend.received_commands[1].gate, Ry)
+
 
 def test_chooser_Ry_reducer():
-    """  
-    Without the chooser_Ry_reducer function, i.e. if 
-    the restricted gate set just picked the first option in
-    each decomposition list, the circuit below would be
-    decomposed into 8 single qubit gates and 1 two qubit gate.
-    Including the Allocate and Measure commands, this 
-    results in 12 commands. 
-    Using the chooser_Rx_reducer you get 9 commands, the 
-    decomposotion having resulted in 4 single qubit gates 
-    and 1 two qubit gate.
-    """
-    engine_list = restrictedgateset.get_engine_list(
-        one_qubit_gates=(Rx, Ry),
-        two_qubit_gates=(Rxx,), compiler_chooser=chooser_Ry_reducer)
-    backend = DummyEngine(save_commands=True)
-    eng = projectq.MainEngine(backend, engine_list, verbose=True)
-    qubit1 = eng.allocate_qubit()
-    qubit2 = eng.allocate_qubit()
-    H | qubit1
-    CNOT | (qubit1, qubit2)
-    Rz(0.2) | qubit1
-    Measure | qubit1
-    assert len(backend.received_commands) < 12
+    # Without the chooser_Ry_reducer function, i.e. if the restricted gate set
+    # just picked the first option in each decomposition list, the circuit
+    # below would be decomposed into 8 single qubit gates and 1 two qubit
+    # gate.
+    #
+    # Including the Allocate, Measure and Flush commands, this would result in
+    # 13 commands.
+    #
+    # Using the chooser_Rx_reducer you get 10 commands, since you now have 4
+    # single qubit gates and 1 two qubit gate.
 
+    for args, count in [(dict(), 13),
+                        ({
+                            'compiler_chooser': chooser_Ry_reducer
+                        }, 11)]:
+
+        engine_list = restrictedgateset.get_engine_list(
+            one_qubit_gates=(Rx, Ry), two_qubit_gates=(Rxx, ), **args)
+        backend = DummyEngine(save_commands=True)
+        eng = projectq.MainEngine(backend, engine_list, verbose=True)
+        qubit1 = eng.allocate_qubit()
+        qubit2 = eng.allocate_qubit()
+        H | qubit1
+        CNOT | (qubit1, qubit2)
+        Rz(0.2) | qubit1
+        Measure | qubit1
+        eng.flush()
+
+        assert len(backend.received_commands) == count
