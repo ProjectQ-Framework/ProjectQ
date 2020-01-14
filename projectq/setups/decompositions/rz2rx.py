30674eb1a5c006fafa71a32fe7acc51baa611155
Damien Nguyen <damien1@huawei.com>
2020-01-14 09:08:15 +0100

    Fix some more files for non-ASCII characters

--- projectq/setups/decompositions/rz2rx.py
+++ projectq/setups/decompositions/rz2rx.py
@@ -12,8 +12,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
-#   Module uses ideas from “Basic circuit compilation techniques for an
-#   ion-trap quantum machine” by Dmitri Maslov (2017) at
+#   Module uses ideas from "Basic circuit compilation techniques for an
+#   ion-trap quantum machine" by Dmitri Maslov (2017) at
 #   https://iopscience.iop.org/article/10.1088/1367-2630/aa5e47
 """
 Registers a decomposition for the Rz gate into an Rx and Ry(pi/2) or Ry(-pi/2)
