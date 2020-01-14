93f2d7938b423c51258ac584c13dcb0f734e9330
David Bretaud <40793394+dbretaud@users.noreply.github.com>
2020-01-14 12:54:44 +0000

    Trapped ion decomposer setup and rotation gates improvement (#346)
    * reduced Ry decomposition : modification of restrictedgateset setup to include compiler chooser, extension of available decompositions, new setup based on restrictedgateset but adapted for trapped ion configurations
    
    * Addition of test file for the rz2rx decomposition
    
    Addition of test file for the rz2rx decomposition and edit to comments in the rz2rx file
    
    * Revert "Addition of test file for the rz2rx decomposition"
    
    This reverts commit 5aab56b7384d60d5ac09e5b08bebf14135b5a006.
    
    * Create rz2rx_test file
    
    Addition of test file for the rz2rx decomposition
    
    * Update rz2rx.py
    
    Update to comments
    
    * Update rz2rx_test.py
    
    Update to comments
    
    * Minor update: remove accidental file
    
    * Minor update rz2rx.py
    
    Corrected an angle.
    
    * Minor update rz2rx_test.py
    
    Edited comments.
    
    * Create h2rx_test.py
    
    Updated method for test_decomposition which tests that decomposition produces identical wave-state up to a global phase.
    
    * Improvement of the decomposition chooser; Note that basic restricted gate
    set will fail decomposing into Rxx because of the default chooser
    
    * Updates to h2rx and cnot2rxx
    
    * Create cnot2rxx_test.py
    
    Testing file for cnot2rxx decomposition. Includes updated method for test_decomposition which tests that decomposition produces identical wave-state up to a global phase.
    
    * basic rotation gates at an angle of 0 or 2pi are removed by the optimizer.
    basic rotation gates ignore now the global phase and are defined over 0:2pi
    
    * Update and create trapped_ion_decomposer_test.py
    
    * Minor update
    
    Documentation and comments.
    
    * Update on comments regarding Identity gates
    
    * Changes 1/2 : command can be printed with unicode symbols only via new
    to_String method; syntax correction;
    
    * Work in progress, is commutable
    
    * Update to decomposition test files
    
    rz2rx_test now tests each decomposition defined in rz2rx.all_defined_decomposition_rules
    Similar for cnot2rxx_test and h2rx_test
    
    * Revert "Work in progress, is commutable"
    
    This reverts commit 27f820cac337c81dbc037b5e3e0381b7e31db5b3.
    
    * minor fixes; revert rotation gates to [0;4pi)
    
    * fix comments
    
    * Fix a few issues with the trapped-ion setup
    
    - Store the sign of the last Ry gate on an engine-by-engine basis
    - Cleanup of some remaining print statements
    - Some stylistic issues fixed
    
    * Mostly fixing stylistic issues and some code cleanup
    
    * h2rx decomposition with correct global phase
    
    * cnot2rxx decomposition with correct global phase
    
    * Fix non-ASCII character in cnot2rxx.py
    
    * Fix some more files for non-ASCII characters
    
    * Specify encoding for files with non-ASCII characters
    
    * Fix test errors
    
    * Fix tests for Python 2.7
    
    * Complete code coverage for trapped-ion setup
    
    Co-authored-by: Nguyen Damien <ngn.damien@gmail.com>

--- projectq/ops/__init__.py
+++ projectq/ops/__init__.py
@@ -25,6 +25,7 @@ from ._basics import (NotMergeable,
 from ._command import apply_command, Command
 from ._metagates import (DaggeredGate,
                          get_inverse,
+                         is_identity,
                          ControlledGate,
                          C,
                          Tensor,
---
+++ projectq/setups/decompositions/rz2rx_test.py
@@ -0,0 +1,125 @@
+#   Copyright 2017 ProjectQ-Framework (www.projectq.ch)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"Tests for projectq.setups.decompositions.rz2rx.py"
+
+import math
+import numpy as np
+import pytest
+
+from projectq import MainEngine
+from projectq.backends import Simulator
+from projectq.cengines import (AutoReplacer, DecompositionRuleSet, DummyEngine,
+                               InstructionFilter)
+from projectq.meta import Control
+from projectq.ops import Measure, Rz
+
+from . import rz2rx
+
+
+def test_recognize_correct_gates():
+    """ Test that recognize_RzNoCtrl recognizes ctrl qubits """
+    saving_backend = DummyEngine(save_commands=True)
+    eng = MainEngine(backend=saving_backend)
+    qubit = eng.allocate_qubit()
+    ctrl_qubit = eng.allocate_qubit()
+    eng.flush()
+    Rz(0.3) | qubit
+    with Control(eng, ctrl_qubit):
+        Rz(0.4) | qubit
+    eng.flush(deallocate_qubits=True)
+    assert rz2rx._recognize_RzNoCtrl(saving_backend.received_commands[3])
+    assert not rz2rx._recognize_RzNoCtrl(saving_backend.received_commands[4])
+
+
+def rz_decomp_gates(eng, cmd):
+    """ Test that cmd.gate is the gate Rz """
+    g = cmd.gate
+    if isinstance(g, Rz):
+        return False
+    else:
+        return True
+
+
+# ------------test_decomposition function-------------#
+# Creates two engines, correct_eng and test_eng.
+# correct_eng implements Rz(angle) gate.
+# test_eng implements the decomposition of the Rz(angle) gate.
+# correct_qb and test_qb represent results of these two engines, respectively.
+#
+# The decomposition only needs to produce the same state in a qubit up to a
+# global phase.
+# test_vector and correct_vector represent the final wave states of correct_qb
+# and test_qb.
+#
+# The dot product of correct_vector and test_vector should have absolute value
+# 1, if the two vectors are the same up to a global phase.
+
+
+@pytest.mark.parametrize("angle", [0, math.pi, 2 * math.pi, 4 * math.pi, 0.5])
+def test_decomposition(angle):
+    """
+    Test that this decomposition of Rz produces correct amplitudes
+
+    Note that this function tests each DecompositionRule in
+    rz2rx.all_defined_decomposition_rules
+    """
+    decomposition_rule_list = rz2rx.all_defined_decomposition_rules
+    for rule in decomposition_rule_list:
+        for basis_state in ([1, 0], [0, 1]):
+            correct_dummy_eng = DummyEngine(save_commands=True)
+            correct_eng = MainEngine(backend=Simulator(),
+                                     engine_list=[correct_dummy_eng])
+
+            rule_set = DecompositionRuleSet(rules=[rule])
+            test_dummy_eng = DummyEngine(save_commands=True)
+            test_eng = MainEngine(backend=Simulator(),
+                                  engine_list=[
+                                      AutoReplacer(rule_set),
+                                      InstructionFilter(rz_decomp_gates),
+                                      test_dummy_eng
+                                  ])
+
+            correct_qb = correct_eng.allocate_qubit()
+            Rz(angle) | correct_qb
+            correct_eng.flush()
+
+            test_qb = test_eng.allocate_qubit()
+            Rz(angle) | test_qb
+            test_eng.flush()
+
+            # Create empty vectors for the wave vectors for the correct and
+            # test qubits
+            correct_vector = np.zeros((2, 1), dtype=np.complex_)
+            test_vector = np.zeros((2, 1), dtype=np.complex_)
+
+            i = 0
+            for fstate in ['0', '1']:
+                test = test_eng.backend.get_amplitude(fstate, test_qb)
+                correct = correct_eng.backend.get_amplitude(fstate, correct_qb)
+                correct_vector[i] = correct
+                test_vector[i] = test
+                i += 1
+
+            # Necessary to transpose vector to use matrix dot product
+            test_vector = test_vector.transpose()
+            # Remember that transposed vector should come first in product
+            vector_dot_product = np.dot(test_vector, correct_vector)
+
+            assert np.absolute(vector_dot_product) == pytest.approx(1,
+                                                                    rel=1e-12,
+                                                                    abs=1e-12)
+
+            Measure | test_qb
+            Measure | correct_qb
