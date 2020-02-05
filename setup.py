473105f21e925d4c02bd433998bd9b639c3e918c
Damien Nguyen <damien1@huawei.com>
2020-02-05 10:38:15 +0100

    Remove global COPYING file and move text to setup.py file itself

--- setup.py
+++ setup.py
@@ -1,3 +1,39 @@
+# Some of the setup.py code is inspired or copied from SQLAlchemy
+
+# SQLAlchemy was created by Michael Bayer.
+
+# Major contributing authors include:
+
+# - Michael Bayer <mike_mp@zzzcomputing.com>
+# - Jason Kirtland <jek@discorporate.us>
+# - Gaetan de Menten <gdementen@gmail.com>
+# - Diana Clarke <diana.joan.clarke@gmail.com>
+# - Michael Trier <mtrier@gmail.com>
+# - Philip Jenvey <pjenvey@underboss.org>
+# - Ants Aasma <ants.aasma@gmail.com>
+# - Paul Johnston <paj@pajhome.org.uk>
+# - Jonathan Ellis <jbellis@gmail.com>
+
+# Copyright 2005-2019 SQLAlchemy authors and contributors (see above)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
 from setuptools import setup, Extension, find_packages
 from distutils.errors import (CCompilerError, DistutilsExecError,
                               DistutilsPlatformError)
@@ -5,7 +41,6 @@ from setuptools import Distribution as _Distribution
 from setuptools.command.build_ext import build_ext
 import sys
 import os
-import setuptools
 import subprocess
 import platform
 
