92ac35d884aea4bf1c324116219910b89801e0dc
Ari Jordan <ari.jordan@gmx.ch>
2020-03-06 12:12:02 +0100

    fix circ drawer when depth == 1

--- projectq/backends/_circuits/_plot.py
+++ projectq/backends/_circuits/_plot.py
@@ -222,12 +222,14 @@ def calculate_gate_grid(axes, qubit_lines, plot_params):
     ]
 
     gate_grid = np.array([0] * (depth + 1), dtype=float)
-
-    gate_grid[0] = plot_params['labels_margin'] + (width_list[0]) * 0.5
-    for idx in range(1, depth):
-        gate_grid[idx] = gate_grid[idx - 1] + column_spacing + (
-            width_list[idx] + width_list[idx - 1]) * 0.5
-    gate_grid[-1] = gate_grid[-2] + column_spacing + width_list[-1] * 0.5
+    
+    gate_grid[0] = plot_params['labels_margin']
+    if depth > 0:
+        gate_grid[0] += width_list[0] * 0.5
+        for idx in range(1, depth):
+            gate_grid[idx] = gate_grid[idx - 1] + column_spacing + (
+                width_list[idx] + width_list[idx - 1]) * 0.5
+        gate_grid[-1] = gate_grid[-2] + column_spacing + width_list[-1] * 0.5
     return gate_grid
 
 
