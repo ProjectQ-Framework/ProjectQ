00468a776704722e8a88c3589a025a1745be9764
Damien Nguyen <damien1@huawei.com>
2020-03-17 08:24:13 +0000

    Minor modifications to _histogram_test.py

--- projectq/libs/hist/_histogram_test.py
+++ projectq/libs/hist/_histogram_test.py
@@ -12,91 +12,91 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from projectq import MainEngine
-from projectq.ops import H, C, X, Measure, All
-from projectq.backends import Simulator
-from projectq.libs.hist import histogram
 import pytest
 import matplotlib
 import matplotlib.pyplot as plt
 
+from projectq import MainEngine
+from projectq.ops import H, C, X, Measure, All
+from projectq.backends import Simulator
+from projectq.libs.hist import histogram
+
+
 @pytest.fixture(scope="module")
 def matplotlib_setup():
     old_backend = matplotlib.get_backend()
-    matplotlib.use('agg') # avoid showing the histogram plots
+    matplotlib.use('agg')  # avoid showing the histogram plots
     yield
     matplotlib.use(old_backend)
+
 
 def test_qubit(matplotlib_setup):
     sim = Simulator()
     eng = MainEngine(sim)
-    qb = eng.allocate_qubit()
+    qubit = eng.allocate_qubit()
     eng.flush()
-    fig, axes, prob = histogram(sim, qb)
+    _, _, prob = histogram(sim, qubit)
     assert prob["0"] == pytest.approx(1)
     assert prob["1"] == pytest.approx(0)
-    H | qb
+    H | qubit
     eng.flush()
-    fig, axes, prob = histogram(sim, qb)
+    _, _, prob = histogram(sim, qubit)
     assert prob["0"] == pytest.approx(0.5)
-    Measure | qb
+    Measure | qubit
     eng.flush()
-    fig, axes, prob = histogram(sim, qb)
+    _, _, prob = histogram(sim, qubit)
     assert prob["0"] == pytest.approx(1) or prob["1"] == pytest.approx(1)
-    plt.show()
 
 
 def test_qureg(matplotlib_setup):
     sim = Simulator()
     eng = MainEngine(sim)
-    qr = eng.allocate_qureg(3)
+    qureg = eng.allocate_qureg(3)
     eng.flush()
-    fig, axes, prob = histogram(sim, qr)
+    _, _, prob = histogram(sim, qureg)
     assert prob["000"] == pytest.approx(1)
-    assert prob ["110"] == pytest.approx(0)
-    H | qr[0]
-    C(X, 1) | (qr[0], qr[1])
-    H | qr[2]
+    assert prob["110"] == pytest.approx(0)
+    H | qureg[0]
+    C(X, 1) | (qureg[0], qureg[1])
+    H | qureg[2]
     eng.flush()
-    fig, axes, prob = histogram(sim, qr)
+    _, _, prob = histogram(sim, qureg)
     assert prob["110"] == pytest.approx(0.25)
     assert prob["100"] == pytest.approx(0)
-    All(Measure) | qr
+    All(Measure) | qureg
     eng.flush()
-    fig, axes, prob = histogram(sim, qr)
+    _, _, prob = histogram(sim, qureg)
     assert prob["000"] == pytest.approx(1) or prob["001"] == pytest.approx(1) \
         or prob["110"] == pytest.approx(1) or prob["111"] == pytest.approx(1)
-    assert prob["000"] + prob["001"] + prob["110"] + prob["111"] == pytest.approx(1)
-    plt.show()
+    assert prob["000"] + prob["001"] + prob["110"] + prob[
+        "111"] == pytest.approx(1)
 
 
 def test_combination(matplotlib_setup):
     sim = Simulator()
     eng = MainEngine(sim)
-    qr = eng.allocate_qureg(2)
-    qb = eng.allocate_qubit()
+    qureg = eng.allocate_qureg(2)
+    qubit = eng.allocate_qubit()
     eng.flush()
-    fig, axes, prob = histogram(sim, [qr, qb])
+    _, _, prob = histogram(sim, [qureg, qubit])
     assert prob["000"] == pytest.approx(1)
-    H | qr[0]
-    C(X, 1)| (qr[0], qr[1])
-    H | qb
-    Measure | qr[0]
+    H | qureg[0]
+    C(X, 1) | (qureg[0], qureg[1])
+    H | qubit
+    Measure | qureg[0]
     eng.flush()
-    fig, axes, prob = histogram(sim, [qr, qb])
+    _, _, prob = histogram(sim, [qureg, qubit])
     assert (prob["000"] == pytest.approx(0.5) and prob["001"] == pytest.approx(0.5)) \
         or (prob["110"] == pytest.approx(0.5) and prob["111"] == pytest.approx(0.5))
     assert prob["100"] == pytest.approx(0)
-    Measure | qb
-    plt.show()
+    Measure | qubit
 
 
 def test_too_many_qubits():
     sim = Simulator()
     eng = MainEngine(sim)
-    qr = eng.allocate_qureg(6)
+    qureg = eng.allocate_qureg(6)
     eng.flush()
-    fig, axes, prob = histogram(sim, qr)
+    _, _, prob = histogram(sim, qureg)
     assert prob["000000"] == pytest.approx(1)
     All(Measure)
-    plt.show()
