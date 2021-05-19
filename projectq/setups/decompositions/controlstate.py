7c5f949644460577d35ac7e96dc6bf7666294007
Damien Nguyen <ngn.damien@gmail.com>
2021-05-19 19:04:07 +0200

    Add some missing license headers and fix some tests

--- projectq/setups/decompositions/controlstate.py
+++ projectq/setups/decompositions/controlstate.py
@@ -1,7 +1,27 @@
+#   Copyright 2021 ProjectQ-Framework (www.projectq.ch)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""
+Register a decomposition to replace turn negatively controlled qubits into positively controlled qubits by applying X
+gates.
+"""
+
+from copy import deepcopy
 from projectq.cengines import DecompositionRule
-from projectq.meta import Control, get_control_count, Compute, Uncompute\
-    , has_negative_control, drop_engine_after
+from projectq.meta import Compute, Uncompute, has_negative_control
 from projectq.ops import BasicGate, X
+
 
 def _decompose_controlstate(cmd):
     """
@@ -16,13 +36,13 @@ def _decompose_controlstate(cmd):
     # Resend the command with the `control_state` cleared
     cmd.ctrl_state = '1' * len(cmd.control_state)
     orig_engine = cmd.engine
-    cmd.engine.receive([cmd])
+    cmd.engine.receive([deepcopy(cmd)])
     Uncompute(orig_engine)
 
+
 def _recognize_offctrl(cmd):
-    return has_negative_control(cmd) == 1
+    return has_negative_control(cmd)
+
 
 #: Decomposition rules
-all_defined_decomposition_rules = [
-    DecompositionRule(BasicGate, _decompose_controlstate, _recognize_offctrl)
-]
+all_defined_decomposition_rules = [DecompositionRule(BasicGate, _decompose_controlstate, _recognize_offctrl)]
--- projectq/setups/decompositions/controlstate_test.py
+++ projectq/setups/decompositions/controlstate_test.py
@@ -1,34 +1,47 @@
+#   Copyright 2021 ProjectQ-Framework (www.projectq.ch)
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
-import pytest
+"""
+Tests for the controlstate decomposition rule.
+"""
 
-import projectq
 from projectq import MainEngine
-from projectq.backends import Simulator
-from projectq.cengines import (AutoReplacer, DecompositionRuleSet, DummyEngine,
-                               InstructionFilter)
-from projectq.meta import Control
-from projectq.ops import All, CNOT, CZ, Measure, X, Z
-
+from projectq.cengines import DummyEngine
+from projectq.meta import Control, has_negative_control
+from projectq.ops import X
 from projectq.setups.decompositions import controlstate
+
+
+def filter_func(eng, cmd):
+    return not has_negative_control(cmd)
 
 
 def test_recognize_gates():
     saving_backend = DummyEngine(save_commands=True)
-    eng = MainEngine(backend=saving_backend, verbose=True)
+    eng = MainEngine(backend=saving_backend, engine_list=[])
     qubit1 = eng.allocate_qubit()
     qubit2 = eng.allocate_qubit()
     qubit3 = eng.allocate_qubit()
-    with Control(eng, qubit2,ctrl_state='0'):
+    with Control(eng, qubit2, ctrl_state='0'):
         X | qubit1
-    with Control(eng, qubit3,ctrl_state='1'):
+    with Control(eng, qubit3, ctrl_state='1'):
         X | qubit1
     eng.flush()  # To make sure gates arrive before deallocate gates
     eng.flush(deallocate_qubits=True)
-    # Don't test initial 4 allocate and flush
-    for cmd in saving_backend.received_commands[2:3]:
-        assert controlstate._recognize_offctrl(cmd)
-    for cmd in saving_backend.received_commands[4:5]:
+
+    for cmd in saving_backend.received_commands[:3]:
         assert not controlstate._recognize_offctrl(cmd)
-
-
-
+    assert controlstate._recognize_offctrl(saving_backend.received_commands[3])
+    for cmd in saving_backend.received_commands[5:]:
+        assert not controlstate._recognize_offctrl(cmd)
