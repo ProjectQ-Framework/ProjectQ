a1b970a1a43a5832051cb0a455370a5932e5f130
Damien Nguyen <ngn.damien@gmail.com>
2021-06-16 15:05:21 +0200

    Fixing a few minor issues

--- projectq/libs/qasm/__init__.py
+++ projectq/libs/qasm/__init__.py
@@ -22,8 +22,6 @@ except ImportError:  # pragma: no cover
     try:
         from ._parse_qasm_pyparsing import read_qasm_file, read_qasm_str
     except ImportError as e:
-        import warnings
-
         err = (
             'Unable to import either qiskit or pyparsing\nPlease install either of them if you want to use '
             'projectq.libs.qasm (e.g. using the command python -m pip install projectq[qiskit])'
