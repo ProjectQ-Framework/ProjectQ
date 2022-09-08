fc75c69627c0dccb87b60961b3f7ba5b15610bef
Damien Nguyen <ngn.damien@gmail.com>
2022-09-08 12:47:10 +0200

    Fix installation on Apple Silicon and older Python versions (disable intrinsics)

--- setup.py
+++ setup.py
@@ -523,6 +523,14 @@ class BuildExt(build_ext):
             '/arch:AVX',
         ]
 
+        if (
+            sys.platform == 'darwin'
+            and platform.machine() == 'arm64'
+            and (sys.version_info.major == 3 and sys.version_info.minor < 9)
+        ):
+            important_msgs('Detected potentially unsupported Python version on Apple Silicon: disabling intrinsics')
+            self.compiler.define_macro('NOINTRIN')
+            return
         if os.environ.get('PROJECTQ_DISABLE_ARCH_NATIVE'):
             flags = flags[1:]
 
