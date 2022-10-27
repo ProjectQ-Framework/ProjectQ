b4b839e32907972458aa54f2539b59a4e5b7f2d2
Christian Clauss <cclauss@me.com>
2022-10-27 10:01:08 +0200

    Context Manager: with flushing(MainEngine()) as eng: (#449)
    * Context Manager: with flushing(MainEngine()) as eng:
    
    Simplify the creation, use, and flushing of any Engine using a Python context manager.
    
    When exiting the `with` block, the engine is _automatically_ flushed.
    
    Modeled after https://docs.python.org/3/library/contextlib.html#contextlib.closing
    ```python
    from projectq import MainEngine, flushing  # import the main compiler engine
    from projectq.ops import (
        H,
        Measure,
    )  # import the operations we want to perform (Hadamard and measurement)
    
    with flushing(MainEngine()) as eng:  # create a default compiler (the back-end is a simulator)
        qubit = eng.allocate_qubit()  # allocate a quantum register with 1 qubit
    
        H | qubit  # apply a Hadamard gate
        Measure | qubit  # measure the qubit
    
        # This line is no longer required... eng.flush()
    
    print(f"Measured {int(qubit)}")  # converting a qubit to int or bool gives access to the measurement result
    
    * Add tests
    
    * Run linters/formatters on code
    
    * Fix isort comment pragmas
    
    * Really fix isort issues in cengines/__init__.py
    
    Co-authored-by: Damien Nguyen <ngn.damien@gmail.com>

--- projectq/cengines/__init__.py
+++ projectq/cengines/__init__.py
@@ -14,9 +14,13 @@
 
 """ProjectQ module containing all compiler engines."""
 
+from contextlib import contextmanager
+
 from ._basics import BasicEngine, ForwarderEngine, LastEngineException  # isort:skip
 from ._cmdmodifier import CommandModifier  # isort:skip
 from ._basicmapper import BasicMapperEngine  # isort:skip
+
+# isort: split
 
 from ._ibm5qubitmapper import IBM5QubitMapper
 from ._linearmapper import LinearMapper, return_swap_depth
@@ -33,3 +37,22 @@ from ._swapandcnotflipper import SwapAndCNOTFlipper
 from ._tagremover import TagRemover
 from ._testengine import CompareEngine, DummyEngine
 from ._twodmapper import GridMapper
+
+
+@contextmanager
+def flushing(engine):
+    """
+    Context manager to flush the given engine at the end of the 'with' context block.
+
+    Example:
+        with flushing(MainEngine()) as eng:
+            qubit = eng.allocate_qubit()
+            ...
+
+    Calling 'eng.flush()' is no longer needed because the engine will be flushed at the
+    end of the 'with' block even if an exception has been raised within that block.
+    """
+    try:
+        yield engine
+    finally:
+        engine.flush()
