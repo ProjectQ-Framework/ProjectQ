3d5f39e86e10d60de7a9960a5354738fc0bc5d8b
Damien Nguyen <ngn.damien@gmail.com>
2024-04-03 01:02:54 +0200

    Test with naked except clause

--- setup.py
+++ setup.py
@@ -239,7 +239,7 @@ def compiler_test(
             with open('err.txt') as err_file:
                 if err_file.readlines():
                     raise RuntimeError('')
-    except (CompileError, LinkError, RuntimeError, DistutilsExecError):
+    except:
         return False
     else:
         return True
