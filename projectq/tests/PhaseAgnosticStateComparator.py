e7bea6f7ce849d73549c6efd22232ec7d238995d
N McDowall <mcdow@MacBook-Pro-105.local>
2025-07-28 22:05:28 -0500

    Add phase-agnostic state comparator and tests (fixes #304)

---
+++ projectq/tests/PhaseAgnosticStateComparator.py
@@ -0,0 +1,26 @@
+import numpy as np
+from projectq import MainEngine
+from projectq.ops import H, CNOT
+from projectq.tests.helpers import PhaseAgnosticStateComparator
+
+def test_hadamard_twice():
+    eng = MainEngine()
+    q = eng.allocate_qureg(1)
+    H | q[0]
+    H | q[0]
+    eng.flush()
+    _, actual = eng.backend.cheat()
+    expected = np.array([1, 0], dtype=complex)
+    comparator = PhaseAgnosticStateComparator()
+    comparator.compare(actual, expected)
+
+def test_bell_state():
+    eng = MainEngine()
+    q = eng.allocate_qureg(2)
+    H | q[0]
+    CNOT | (q[0], q[1])
+    eng.flush()
+    _, actual = eng.backend.cheat()
+    expected = np.array([1/np.sqrt(2), 0, 0, 1/np.sqrt(2)], dtype=complex)
+    comparator = PhaseAgnosticStateComparator()
+    comparator.compare(actual, expected)
